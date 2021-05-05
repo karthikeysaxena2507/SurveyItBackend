16539f6522572d472b81099ab8db548062e85c16
karthikeysaxena <karthikeysaxena@gmail.com>
2021-05-05 09:45:06 +0530

    testing

--- src/main/java/survey/controller/UserController.java
+++ src/main/java/survey/controller/UserController.java
@@ -28,7 +28,7 @@ import survey.payload.LoginRequest;
 import survey.payload.MessageResponse;
 import survey.payload.SignupRequest;
 
-@CrossOrigin(origins = "https://survey-it.netlify.app/")
+@CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
