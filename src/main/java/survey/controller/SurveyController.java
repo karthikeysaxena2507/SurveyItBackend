ad14fe24603f8cfbfaaf1396b350daff744b3fbc
karthikeysaxena <karthikeysaxena@gmail.com>
2021-05-07 20:01:31 +0530

    Adding to heroku

--- src/main/java/survey/controller/SurveyController.java
+++ src/main/java/survey/controller/SurveyController.java
@@ -7,7 +7,6 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
